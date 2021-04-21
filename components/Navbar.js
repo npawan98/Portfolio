b53219896f61f78654a820de99f09517479e76ff
npawan98 <npawan98>
2021-04-21 16:40:27 +0530

    initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+function Navbar() {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default Navbar
+
+const Container = styled.div`
+
+`;
+
